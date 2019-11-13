df892fe223e2e95dad65066409253f23175d757b
kabl <kasimir@digitalizeit.io>
2019-11-13 18:13:41 +0100

    vesting can spend their dividends

---
+++ test/DividendAndVestingTest.js
@@ -0,0 +1,44 @@
+const BMTToken = artifacts.require("./BMTToken.sol");
+
+contract("BMTToken DividendAndVestingTest test", async accounts => {
+
+    const SECONDS_IN_DAY = 86400;
+
+    const BrickMark = accounts[0];
+    const alice = accounts[5];
+    const bob = accounts[6];
+    const carol = accounts[7];
+
+    it("Mint Vested, Pay Dividend, spend dividend", async () => {
+        let instance = await BMTToken.new({from: BrickMark});
+        let amount = "1000000000000000000";
+        
+        let now = Math.trunc(new Date().getTime() / 1000);
+        console.log("time now: " + now);
+        let lockTime = now + SECONDS_IN_DAY;
+
+        await instance.mintBatchVested([alice], [amount], [lockTime]);
+        
+        let spendable = await instance.spendableAmount.call(alice);
+        let balance = await instance.balanceOf.call(alice);
+        assert.equal(spendable, 0);
+        assert.equal(balance.toString(), amount);
+
+        await instance.payDividend([alice], [amount], "First Dividend");
+        spendable = await instance.spendableAmount.call(alice);
+        balance = await instance.balanceOf.call(alice);
+        assert.equal(spendable.toString(), amount);
+        assert.equal(balance.toString(), "2000000000000000000");
+
+        await instance.transfer(bob, amount, {from: alice})
+        spendable = await instance.spendableAmount.call(alice);
+        balance = await instance.balanceOf.call(alice);
+        assert.equal(spendable.toString(), 0);
+        assert.equal(balance.toString(), amount);
+
+        spendable = await instance.spendableAmount.call(bob);
+        balance = await instance.balanceOf.call(bob);
+        assert.equal(spendable.toString(), amount);
+        assert.equal(balance.toString(), amount);
+    });
+});
