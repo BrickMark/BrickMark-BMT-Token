7b2e254794fbe2794bac13fc731046fbf1793d4d
kabl <kasimir@digitalizeit.io>
2019-12-17 11:28:40 +0100

    navigation first step

--- test-ui/src/App.vue
+++ test-ui/src/App.vue
@@ -1,23 +1,65 @@
 <template>
-    <v-app>
-        <Header />
-    </v-app>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <router-link to="/">
+              <v-list-item-title>BMT Overview</v-list-item-title>
+            </router-link>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <router-link to="/about">
+              <v-list-item-title>About</v-list-item-title>
+            </router-link>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="indigo" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="grey--text" />
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light white--text">BMT</span>
+        <span>Manager</span>
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <router-view />
+    </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import Header from "./components/layout/Header.vue";
-
 export default {
   name: "App",
-  components: {
-    Header
+  props: {
+    source: String
   },
-  created: function () {
+  data: () => ({
+    drawer: null,
+    links: [
+      {icon: "dashboard", text: "Dashboard", route: "/"},
+      {icon: "about", text: "About", route: "/about"}
+    ]
+  }),
+  created: function() {
     console.log("App loaded");
     this.$store.dispatch("updateBmtInfo");
     this.$store.dispatch("updateAllUsers", this.$store.getters.users);
   }
-  
 };
 </script>
 
---
+++ test-ui/src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav>
+    <v-toolbar flat app>
+      <v-toolbar-side-icon @click="drawer = !drawer" class="grey--text"></v-toolbar-side-icon>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">BMT</span>
+        <span>Manager</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat color="grey">
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-navigation-drawer app v-model="drawer" class="primary">
+      <v-list>
+        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-item-action>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      links: [
+        {icon: "dashboard", text: "Dashboard", route: "/"},
+        {icon: "about", text: "About", route: "/about"}
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
